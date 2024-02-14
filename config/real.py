21699194188a8282083c31ef2d2004f24f2d4d35
MadPumpkin3 <fpdjxpa37@gmail.com>
2024-02-14 09:14:18 +0000

    domain in settings.py

--- config/real.py
+++ config/real.py
@@ -3,4 +3,4 @@ from .settings import *
 DEBUG = True
 
 # DEBUG가 False 경우, ALLOWED_HOSTS에 공인 IP를 넣어야 한다.
-ALLOWED_HOSTS = ['3.35.235.165'] # 클라우드 web or was IP, 도메인 차후 추가
+ALLOWED_HOSTS = ['13.209.82.108'] # 클라우드 web or was IP, 도메인 차후 추가
