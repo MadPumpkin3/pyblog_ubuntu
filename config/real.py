5a80cc529871d5f163e92db9e1a31a829f4fb5d4
MadPumpkin3 <fpdjxpa37@gmail.com>
2024-02-17 15:46:37 +0900

    models reset

--- config/real.py
+++ config/real.py
@@ -3,4 +3,4 @@ from .settings import *
 DEBUG = False
 
 # DEBUG가 False 경우, ALLOWED_HOSTS에 공인 IP를 넣어야 한다.
-ALLOWED_HOSTS = ['43.203.172.120'] # 클라우드 web or was IP, 도메인 차후 추가
+ALLOWED_HOSTS = ['3.38.162.65'] # 클라우드 web or was IP, 도메인 차후 추가
--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class PyBlog(models.Model):
-    id = models.AutoField(primary_key=True, null=False)
-    title = models.CharField(max_length=100)
-    update_dt = models.DateTimeField(auto_now=True)
-    regist_dt = models.DateTimeField(auto_now_add=True)
+# class PyBlog(models.Model):
+#     id = models.AutoField(primary_key=True, null=False)
+#     title = models.CharField(max_length=100)
+#     update_dt = models.DateTimeField(auto_now=True)
+#     regist_dt = models.DateTimeField(auto_now_add=True)
     
-    class Meta:
-        db_table = 'py_blog'
+#     class Meta:
+#         db_table = 'py_blog'
         
-class PyBlogDetail(models.Model):
-    detail = models.ForeignKey(PyBlog, models.DO_NOTHING, related_name='pb_detail')
-    sub_title = models.CharField(max_length=100, blank=True, null=True)
-    img_url = models.CharField(max_length=200, blank=True, null=True)
-    img_size = models.CharField(max_length=3, blank=True, null=True)
-    content_body = models.TextField(blank=True, null=True)
+# class PyBlogDetail(models.Model):
+#     detail = models.ForeignKey(PyBlog, models.DO_NOTHING, related_name='pb_detail')
+#     sub_title = models.CharField(max_length=100, blank=True, null=True)
+#     img_url = models.CharField(max_length=200, blank=True, null=True)
+#     img_size = models.CharField(max_length=3, blank=True, null=True)
+#     content_body = models.TextField(blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'py_blog_detail'
+#     class Meta:
+#         managed = False
+#         db_table = 'py_blog_detail'
