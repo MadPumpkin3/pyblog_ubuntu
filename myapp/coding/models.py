e206b9eb748db8d458a9278cca36be4d5248c82b
MadPumpkin3 <fpdjxpa37@gmail.com>
2024-02-24 18:26:43 +0900

    modify models.py and talbl

--- myapp/coding/models.py
+++ myapp/coding/models.py
@@ -3,10 +3,11 @@ from django.urls import reverse
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 from treebeard.mp_tree import MP_Node
+from django.utils.functional import cached_property
 
 # Create your models here.
 
-IMG_SIZE_CHOICES = {(30, '30%'), (40, '40%'), (50, '50%'), (70, '70%'), (100, '100%')}
+IMG_SIZE_CHOICES = [(30, '30%'), (40, '40%'), (50, '50%'), (70, '70%'), (100, '100%')]
 
 class PyCoding(MP_Node):
     id = models.AutoField(primary_key=True)
@@ -18,18 +19,20 @@ class PyCoding(MP_Node):
     # content_body = models.TextField(null=False, blank=False)
     content_body = MarkdownxField()
     update_dt = models.DateTimeField(auto_now=True)
-    regist_dt = models.DateTimeField(auto_now_add=True)
+    regist_dt = models.DateTimeField(auto_now_add=False)
     
     node_order_by = ['regist_dt']
     
     def get_absolute_url(self):
         if not self.is_root():
             return reverse("coding:coding_detail", kwargs={"pk": self.pk})
-        
+    
+    @cached_property
     def get_previous(self):
         if not self.is_root():
             return self.get_prev_sibling()
-        
+    
+    @cached_property
     def get_next(self):
         if not self.is_root():
             return self.get_next_sibling()
